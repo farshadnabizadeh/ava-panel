8fcc82e866b0437d4ca4a099f0f8d0e8a351dd29
Farshad Nabizadeh <farshadnabizadeh.crux@gmail.com>
2023-06-02 07:23:03 -0700

    master

--- processes/processes.js
+++ processes/processes.js
@@ -8,16 +8,19 @@ const route = () => {
     var pathname = window.location.pathname;
     var component = (window.location.href.split('?route='))[1]
     switch (component) {
-        case 'login':
-            loginComponent(pathname, 'loginComponent.html')
+        case 'signin':
+            render(origin, pathname, 'loginComponent.html')
+            break
+        case 'signup':
+            render(route, 'registerComponent.html')
             break
         default:
             console.log('Not Found')
             break;
     }
 }
-const loginComponent = (pathname, componentName) => {
-    $.get(`${pathname}Components/${componentName}`, function (data) {
-        $('#root').html(data);
-    }, 'html');
+
+
+const render = (origin, pathname, componentName) => {
+    $('#root').load(origin + pathname + '/Components/' + componentName)
 }
