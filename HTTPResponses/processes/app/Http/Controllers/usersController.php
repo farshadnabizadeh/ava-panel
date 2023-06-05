0a2ae7cb8a6fc30696329557b6ba36073218b6da
farshad Nabizadeh <farshadn@hotelistan.com>
2023-06-05 01:34:02 -0700

    passwordValidation

--- HTTPResponses/processes/app/Http/Controllers/usersController.php
+++ HTTPResponses/processes/app/Http/Controllers/usersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Response;
+use App\Models\User;
 
 class usersController extends Controller
 {
@@ -11,7 +12,12 @@ class usersController extends Controller
          $errors = array();
          $nextprocess = array();
          if(isset($request->username)) {
-            $nextprocess['username'] = true;
+            if($this->uniqueUsername($request->username)){
+                $nextprocess['username'] = true;
+            }else{
+                array_push($errors,'username already exists');
+                $nextprocess['username'] = false;
+            }
          }else{
             array_push($errors,'username is required');
             $nextprocess['username'] = false;
@@ -28,7 +34,12 @@ class usersController extends Controller
             $nextprocess['email'] = false;
          }
          if(isset($request->password)){
-            $nextprocess['password'] = true;
+             if($this->passwordCheck($request->password)){
+                $nextprocess['password'] = true;
+             }else{
+                array_push($errors,'Password must be 8 Charachter');
+                $nextprocess['password'] = false;
+             }
          }else{
             array_push($errors,'password is required');
             $nextprocess['password'] = false;
@@ -51,5 +62,19 @@ class usersController extends Controller
     }
     public function emailValidation($email){
         return filter_var($email, FILTER_VALIDATE_EMAIL) && preg_match('/@.+\./', $email);
+    }
+    public function uniqueUsername($username){
+        if(User::where('username',$username)->count()==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public function passwordCheck($password){
+        if(strlen($password)==8){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
